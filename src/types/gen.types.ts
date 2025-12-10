c669789a8ffb922d0bbf9972917aba94a72eb50e
2qp <56475797+2qp@users.noreply.github.com>
2025-12-10 12:35:44 +0530

    chore(types): add `TNameId` | `SegmentDef`

--- src/types/gen.types.ts
+++ src/types/gen.types.ts
@@ -6,4 +6,8 @@ type Flex<T extends ElementBase = "string"> = `${T} & {}`;
 
 type EleType<T extends Primitive, TBase extends ElementBase = "string"> = `((${T}) | (${Flex<TBase>}))` | `(${T})`;
 
-export type { EleType, Primitive };
+type TNameId<TName extends string> = `${TName}_${number}`;
+
+type SegmentDef<T extends Primitive, TName extends string> = `const ${TNameId<TName>} = [${T}, ${T}] as const;`;
+
+export type { EleType, Primitive, SegmentDef, TNameId };
